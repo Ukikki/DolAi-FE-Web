ecbbbf814de913a2225c59232a82bc48332ca0de
felizsong <jalonemo@naver.com>
2025-04-13 13:47:32 +0900

    🎨토스트 알림 색상 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -14,7 +15,12 @@ export default defineConfig({
   define: {
     global: 'window',
   },
-  
+  resolve: {
+    alias : {
+      "@": path.resolve(__dirname, "src")
+    }
+  },
+
   plugins: [react()],
 })
 
